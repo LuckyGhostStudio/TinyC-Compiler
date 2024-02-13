c2557ef994d30c8b4c83171995322b44a2513f3d
LuckyGhostStudio <1398346167@qq.com>
2024-02-13 23:29:52 +0800

    Add Token Structures

--- Core/Source/Core/Compiler.h
+++ Core/Source/Core/Compiler.h
@@ -4,6 +4,49 @@
 
 namespace Compiler
 {
+	struct Position
+	{
+		int line;
+		int column;
+		const char* fileName;
+	};
+
+	/// <summary>
+	/// Token类型
+	/// </summary>
+	enum class TokenType
+	{
+		Identifier,	// 标识符
+		Keyword,	// 关键字	int float ...
+		Operator,	// 运算符	+ - * / ...
+		Symbol,		// 符号		; : " ...
+		Number,		// 数字		0 1 2 3 ...
+		String,		// 字符串	"Hello"
+		Comment,	// 注释		// ... /* ... */
+		NewLine		// 新行
+	};
+
+	struct Token
+	{
+		TokenType type;
+		int flags;
+
+		/* token 值 */
+		union
+		{
+			char charValue;						// char 类型值
+			const char* stringValue;			// string 类型值
+			unsigned int intNumber;				// int 类型值
+			unsigned long longNumber;			// long 类型值
+			unsigned long long longlongNumber;	// longlong 类型值
+			void* any;							// 其他类型值
+		};
+
+		bool whiteSpace;				// 当前token和下一个token之间是否存在空白（空格或别的空白符）
+
+		const char* between_brackets;	// 括号之间的字符串值（调试用）
+	};
+
 	enum class CompileState
 	{
 		FileCompiledOK,		// 文件编译成功
